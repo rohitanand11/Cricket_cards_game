c8f9af8db079bed1b97b589da432f7a389f41e1d
Rohit Anand <rohitanand3456@gmail.com>
2020-06-25 18:15:38 +0530

    added homeage and added build i docs folder for deployment

---
+++ docs/precache-manifest.347c979f93dc8e8fb9363bc091567c9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bbcc215b6eb849aa48ab634d290eac",
+    "url": "/Cricket_cards_game/index.html"
+  },
+  {
+    "revision": "ce49c63d517a5222784d",
+    "url": "/Cricket_cards_game/static/css/main.409917db.chunk.css"
+  },
+  {
+    "revision": "2205fd308519a1410e09",
+    "url": "/Cricket_cards_game/static/js/2.6ad7678e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cricket_cards_game/static/js/2.6ad7678e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce49c63d517a5222784d",
+    "url": "/Cricket_cards_game/static/js/main.c96acd53.chunk.js"
+  },
+  {
+    "revision": "c192600121387e5380a2",
+    "url": "/Cricket_cards_game/static/js/runtime-main.f3847735.js"
+  },
+  {
+    "revision": "f3648ab724f97314f3965023a8bbcdf5",
+    "url": "/Cricket_cards_game/static/media/3.f3648ab7.jpg"
+  },
+  {
+    "revision": "7643de4bf4034f0cdd4d7b5aaaacb486",
+    "url": "/Cricket_cards_game/static/media/4.7643de4b.jpg"
+  },
+  {
+    "revision": "a1d557390e19ac3d1c733a25dd31d81c",
+    "url": "/Cricket_cards_game/static/media/6.a1d55739.jpg"
+  },
+  {
+    "revision": "56c3d18e993f32723312542dc0568255",
+    "url": "/Cricket_cards_game/static/media/7.56c3d18e.jpg"
+  },
+  {
+    "revision": "58da861d48dcfcfccf4fdb8b47be7ecf",
+    "url": "/Cricket_cards_game/static/media/8.58da861d.jpg"
+  }
+]);
